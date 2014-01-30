bc571e65c9cc2b0d9f93f0fdc9a3e66b49d2baa6
Chris Abad <chrisabad@gmail.com>
2014-01-29 17:19:55 -0800

    Hooked up all the forms.

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,9 +1,37 @@
 // File for hijacking submission forms.
 
-$("form").live('submit', function(e) {
+$('#subscribe-form').submit(function() {
+	try {
+		var form = $(this);
+		var email = form.find('input[name="email"]').val();
+		var name = form.attr('name');
 
-    e.preventDefault();
-    e.returnValue = false;
+		// Swap the message
+		form.hide();
+		$('#success-message').show();
 
-    console.log('You submitted the form!');
+		if (email == '') {
+			console.log('Email was blank.');
+	    } else {
+	    	console.log('Identifying visitor as ' + email + '.');
+	    	
+	    	var user = analytics.user(); 
+			var id = user.id();
+
+			// only alias user if they are anonymous
+			console.log('ID: ' + id);
+			console.log('Email:' + email);
+			if (id != email) {
+				analytics.alias(email);
+			}
+
+			// the identified user is identified with traits
+			analytics.identify(email, { email: email });
+			analytics.track('Signed up for ' + name);
+	    }
+		return false;
+	} catch (error) {
+		console.log('There was an error.' + error);
+		return false;
+	}
 });
