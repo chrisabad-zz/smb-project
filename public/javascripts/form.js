a954b52e75f5ff5057187e887651016f567f9dfd
Doug Fulop <dougfulop@gmail.com>
2014-02-04 12:13:50 -0800

    added feedback to ensemble

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -8,7 +8,9 @@ $('#subscribe-form').submit(function() {
 
 		// Swap the message
 		form.hide();
-		$('#success-message').show();
+		// $('#success-message').show();
+		$('#feedback-request').show();
+		$('#subscribe-text').text("What do you think about the product?")
 
 		if (email == '') {
 			console.log('Email was blank.');
@@ -38,6 +40,34 @@ $('#subscribe-form').submit(function() {
 	}
 });
 
+$('#feedback-form').submit(function() {
+	try {
+		var form = $(this);
+		var content = form.find('input[id="feedback"]').val();
+		var name = form.attr('name');
+
+		// Swap the message
+		form.hide();
+		$('#success-message').show();
+
+		if (content == '') {
+			console.log('Feedback was blank.');
+	    } else {
+	    	console.log('Recording content as ' + content + '.');
+	    	
+	    	var user = analytics.user(); 
+			var id = user.id() || 0;
+
+			var myRootRef = new Firebase('https://dazzling-fire-6599.firebaseio.com/feedbacks');
+			myRootRef.push({concept: name, text: content, user: id});
+	    }
+		return false;
+	} catch (error) {
+		console.log('There was an error.' + error);
+		return false;
+	}
+});
+
 $('#competitor-form').submit(function() {
 	try {
 		var form = $(this);
@@ -57,19 +87,6 @@ $('#competitor-form').submit(function() {
 	    	
 	    	var user = analytics.user(); 
 			var id = user.id() || 0;
-
-			// only alias user if they are anonymous
-			console.log('ID: ' + id);
-			// console.log('Email:' + email);
-			// if (id != email) {
-			//	analytics.alias(email);
-			// }
-
-			// the identified user is identified with traits
-			// traits = { email: email, name: email }
-			// traits[name] = true;
-			// analytics.identify(email, traits);
-			// analytics.track('Signed up for ' + name);
 
 			var myRootRef = new Firebase('https://dazzling-fire-6599.firebaseio.com/feedbacks');
 			myRootRef.push({concept: product, text: content, user: id});
