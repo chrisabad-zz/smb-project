d95ef36b59ecdfd7821e1ec64730228f2644560b
trive <mtriverio@Dieter.local>
2014-02-04 16:30:32 -0800

    Sign up fixes

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -40,15 +40,82 @@ $('#subscribe-form').submit(function() {
 	}
 });
 
+$('#subscribe-form-bottom').submit(function() {
+	try {
+		var form = $(this);
+		var email = form.find('input[name="email"]').val();
+		var name = form.attr('name');
+
+		// Swap the message
+		form.hide();
+		$('#feedback-request-bottom').show();
+		$('#subscribe-text-bottom').text("What do you think about the product?")
+
+		if (email == '') {
+			console.log('Email was blank.');
+	    } else {
+	    	console.log('Identifying visitor as ' + email + '.');
+	    	
+	    	var user = analytics.user(); 
+			var id = user.id();
+
+			// only alias user if they are anonymous
+			console.log('ID: ' + id);
+			console.log('Email:' + email);
+			if (id != email) {
+				analytics.alias(email);
+			}
+
+			// the identified user is identified with traits
+			traits = { email: email, name: email }
+			traits[name] = true;
+			analytics.identify(email, traits);
+			analytics.track('Signed up for ' + name);
+	    }
+		return false;
+	} catch (error) {
+		console.log('There was an error.' + error);
+		return false;
+	}
+});
+
 $('#feedback-form').submit(function() {
 	try {
 		var form = $(this);
-		var content = form.find('input[id="feedback"]').val();
+		var content = form.find('textarea[id="feedback"]').val();
 		var name = form.attr('name');
 
 		// Swap the message
 		form.hide();
 		$('#success-message').show();
+
+		if (content == '') {
+			console.log('Feedback was blank.');
+	    } else {
+	    	console.log('Recording content as ' + content + '.');
+	    	
+	    	var user = analytics.user(); 
+			var id = user.id() || 0;
+
+			var myRootRef = new Firebase('https://dazzling-fire-6599.firebaseio.com/feedbacks');
+			myRootRef.push({concept: name, text: content, user: id});
+	    }
+		return false;
+	} catch (error) {
+		console.log('There was an error.' + error);
+		return false;
+	}
+});
+
+$('#feedback-form-bottom').submit(function() {
+	try {
+		var form = $(this);
+		var content = form.find('textarea[id="feedback-bottom"]').val();
+		var name = form.attr('name');
+
+		// Swap the message
+		form.hide();
+		$('#success-message-bottom').show();
 
 		if (content == '') {
 			console.log('Feedback was blank.');
